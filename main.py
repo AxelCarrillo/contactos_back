bcc6c332fcf4540a1d72ae0f5e97656c4c37a5e3
Axel Carrillo <1722110634@utectulancingo.edu.mx>
2023-12-02 06:17:30 +0000

    ou

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ conn = sqlite3.connect("sql/contactos.db")
 app = fastapi.FastAPI()
 
 origins = [
-    "https://contactos-frontend-ajio.onrender.com"
+    "https://contactos-backend-2f1e8da37e9f.herokuapp.com"
 ]
 
 app.add_middleware(
